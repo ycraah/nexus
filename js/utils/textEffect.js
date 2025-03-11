b51bbb8857c6622f9f89b06dc72a34c47333089c
ycraah <ycraah@gmail.com>
2025-03-11 21:38:53 +0900

    li 계속 늘어나는 문제 방지 -추가 필요

--- js/utils/textEffect.js
+++ js/utils/textEffect.js
@@ -27,14 +27,13 @@ const inputText = (() => {
     const textBeltTextArr = ["web design", "illustration", "development", "logo & branding", "mobile app design"];
     const upperCaseArr = textBeltTextArr.map(text => text.toUpperCase());
     const textBelt = document.querySelector(".section__about-us .text-belt__list");
-    setInterval(()=> {
         for(let i = 0; i < upperCaseArr.length; i++){
             const $li = document.createElement("li");
             const textNode = document.createTextNode(upperCaseArr[i]);
             $li.appendChild(textNode);
             textBelt.appendChild($li);
         }
-    }, 1000)
+    
 }) 
 
 export {textButtomUpRotate, textButtomUpOpacity, inputText}
